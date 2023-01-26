d7c3b9366d6bcdb80706e576eb368b4c16495da2
Vineela Yetrintala <Vineela.yetrintala@gmail.com>
2023-01-26 17:27:27 -0600

    new changes

--- students/models.py
+++ students/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class Student(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+
+    def __str__(self) -> str:
+        return self.first_name + " " + self.last_name
