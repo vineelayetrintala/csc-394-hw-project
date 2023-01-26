90b790d30bcb824e5e6d742b5338d09a1855d352
Vineela Yetrintala <Vineela.yetrintala@gmail.com>
2023-01-26 16:19:42 -0600

    update setting.py password

--- homework/settings.py
+++ homework/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'Depaul23*',
+        'PASSWORD': 'postgres',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
